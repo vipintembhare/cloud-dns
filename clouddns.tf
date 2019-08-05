721412ed38af7c6a844fd8798a096b037c84f123
vipintembhare <vipinrtembhare@gmail.com>
2019-08-05 16:25:56 +0530

    cloud dns

---
+++ clouddns.tf
@@ -0,0 +1,13 @@
+resource "google_dns_record_set" "frontend" {
+  name = "${google_dns_managed_zone.prod.dns_name}"
+  type = "A"
+  ttl  = 300
+
+  managed_zone = "${google_dns_managed_zone.dev.name}"
+
+  rrdatas = ["35.224.18.23"]
+}
+resource "google_dns_managed_zone" "dev" {
+  name     = "dev"
+  dns_name = "config-server-service."
+}
